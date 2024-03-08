3a38ce1ee3060b781c99db7e3a75a4b947e1efba
Colin Woodbury <colin@fosskers.ca>
2024-03-08 14:28:53 +0900

    feat: write to the db quickly

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,67 @@
 use gumdrop::{Options, ParsingStyle};
+use sqlx::{prelude::FromRow, sqlite::SqlitePoolOptions};
+use std::time::Duration;
 
 /// Hit an SQLite database.
 #[derive(Debug, Options)]
 struct Args {
     /// The identity of this executable at runtime.
     #[options(required)]
-    id: usize,
+    id: u32,
 }
 
-fn main() {
-    let args = Args::parse_args_or_exit(ParsingStyle::AllOptions);
+#[derive(Debug, FromRow)]
+struct Row {
+    id: u32,
+    exec: u32,
+}
 
-    println!("ID: {}", args.id);
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let args = Args::parse_args_or_exit(ParsingStyle::AllOptions);
+    println!("Binary ID: {}", args.id);
+
+    tokio::time::sleep(Duration::from_secs(3)).await;
+
+    println!("Going!");
+
+    let pool = SqlitePoolOptions::new()
+        .max_connections(6)
+        .connect("test.db")
+        .await?;
+
+    println!(
+        "Acquire Timeout: {}",
+        pool.options().get_acquire_timeout().as_millis()
+    );
+
+    let res = sqlx::query_as::<_, Row>("select * from smack")
+        .fetch_all(&pool)
+        .await?;
+
+    println!("Before: {}", res.len());
+
+    for _ in 0..1000 {
+        let pule = pool.clone();
+
+        tokio::spawn(async move {
+            if let Err(e) = sqlx::query("insert into smack (exec) values (?)")
+                .bind(args.id)
+                .execute(&pule)
+                .await
+            {
+                eprintln!("Error: {}", e);
+            }
+        });
+    }
+
+    tokio::time::sleep(Duration::from_secs(10)).await;
+
+    let res = sqlx::query_as::<_, Row>("select * from smack")
+        .fetch_all(&pool)
+        .await?;
+
+    println!("After: {}", res.len());
+
+    Ok(())
 }
